6b39b8489342b1874eae972f491352aa1396a558
AndrewShapoval <andrewshapoval88@gmail.com>
2020-10-14 16:04:24 +0300

    application structure

---
+++ src/n1-main/m1-ui/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Header} from "./header/Header";
+import {Routes} from "./header/Routes/Routes";
+
+export const Main = () => {
+    return (
+        <div>
+            <Header/>
+            <Routes/>
+        </div>
+    )
+}
---
+++ src/n1-main/m1-ui/Main/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div>
+            Проект на миллион готов к сдаче!
+        </div>
+    )
+}
