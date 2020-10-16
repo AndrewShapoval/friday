cbd3396d20009aba4cc8daf124a70c4d6ddcbb45
AndrewShapoval <andrewshapoval88@gmail.com>
2020-10-16 17:55:02 +0300

    Updates

---
+++ precache-manifest.1c018d049fc42538e55ed1ee705966e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae411cb7e6ca260a11f0c4a332a3133",
+    "url": "/friday/index.html"
+  },
+  {
+    "revision": "3bd3ca8302d59bafbf2b",
+    "url": "/friday/static/css/main.0b50408f.chunk.css"
+  },
+  {
+    "revision": "910a3d3b3866b4d6d350",
+    "url": "/friday/static/js/2.340fb957.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/friday/static/js/2.340fb957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd3ca8302d59bafbf2b",
+    "url": "/friday/static/js/main.ff89b4fd.chunk.js"
+  },
+  {
+    "revision": "92b6092d1c12367e3374",
+    "url": "/friday/static/js/runtime-main.79b15118.js"
+  }
+]);
